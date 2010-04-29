9264eaa8b96324653dfd6e247fca98af4b3423bf
Arvid Jakobsson <arvid@arvid-desktop-gluteus-lvlup.(none)>
2010-04-29 09:18:11 +0200

    Better comment-or-uncomment.

--- rejeep-comment.el
+++ rejeep-comment.el
@@ -1,27 +1,15 @@
 ;;; rejeep-comment.el --- Helpers for commenting/uncommenting stuff
 
 (defun comment-or-uncomment-current-line-or-region ()
-  "Comments or uncomments current current line or whole lines in
-region (if any)."
+  "Comments or uncomments current current line or whole lines in region."
   (interactive)
   (save-excursion
-    (if mark-active
-        (mark-whole-lines-region)
-      (mark-current-line))
-    (comment-or-uncomment-region (region-beginning) (region-end))))
-
-(defun mark-current-line ()
-  "Marks current line, except whitespace at the beginning."
-  (interactive)
-  (set-mark (line-end-position))
-  (back-to-indentation))
-
-(defun mark-whole-lines-region ()
-  "Marks whole lines in the region."
-  (if (> (point) (mark))
-      (exchange-point-and-mark))
-  (beginning-of-line)
-  (exchange-point-and-mark)
-  (end-of-line))
+    (let (min max)
+      (if (and transient-mark-mode mark-active)
+          (setq min (region-beginning) max (region-end))
+        (setq min (point) max (point)))
+      (comment-or-uncomment-region
+       (progn (goto-char min) (line-beginning-position))
+       (progn (goto-char max) (line-end-position))))))
 
 (provide 'rejeep-comment)
