3e84c8e0c53da7b2dca04b7891adb0b0a716467f
Arvid Jakobsson <arvid@arvid-desktop-gluteus-lvlup.(none)>
2010-08-18 10:16:35 +0200

    Binding for magit-status.

--- arvid-magit.el
+++ arvid-magit.el
@@ -5,4 +5,6 @@
             (set-face-foreground 'magit-diff-add "green3")
             (set-face-foreground 'magit-diff-del "red3")))
 
+(global-set-key (kbd "C-x G") 'magit-status)
+
 (provide 'arvid-magit)
