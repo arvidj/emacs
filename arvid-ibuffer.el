41486f122420c6be1fc88580a3606c76f2abe545
Arvid Jakobsson <arvid@arvid-desktop-gluteus-lvlup.(none)>
2010-09-06 09:13:43 +0200

    Add more ibuffer filters and activate auto-mode.

--- arvid-ibuffer.el
+++ arvid-ibuffer.el
@@ -16,15 +16,25 @@
                 (filename . "remote/dev/framtidensmat"))
                ("wwoof-dev"
                 (filename . "remote/dev/wwoof_community"))
+			   ("core-arvid-local"
+                (filename . "public_html/core-arvid-local"))
+			   ("core-arvid-local"
+                (filename . "public_html/core-magenta-2.0-git"))
+			   ("magit"
+                (name . "^\\*magit"))
+			   ("dired"
+                (mode . dired-mode))
                ("svn"
                 (name . "svn"))
                ("sql"
                 (name . "*SQL*"))
                ))))
 
+(setq ibuffer-show-empty-filter-groups nil)
 (add-hook 'ibuffer-mode-hook
-          (lambda () 
-            (ibuffer-switch-to-saved-filter-groups "default")))
+          (lambda ()
+			(ibuffer-auto-mode 1)
+			(ibuffer-switch-to-saved-filter-groups "default")))
 
 (define-key ibuffer-mode-map (kbd "TAB") 'ibuffer-toggle-filter-group)
 ;; TODO: in ibuffer, use ido to switch file but it should default to dir of buffer on point
