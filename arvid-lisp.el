504e83b85241e92fda8fbf2258c3663961d9ce56
Arvid Jakobsson <arvid@arvid-desktop-gluteus-lvlup.(none)>
2010-09-06 09:11:52 +0200

    RET is newline and indent in lisp-mode.

--- arvid-lisp.el
+++ arvid-lisp.el
@@ -2,6 +2,8 @@
 (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
 
 (add-hook 'emacs-lisp-mode-hook 
-	  '(lambda () (define-key emacs-lisp-mode-map (kbd "C-c C-l") 'eval-buffer)))
+	  '(lambda () 
+		 (define-key emacs-lisp-mode-map (kbd "RET") 'newline-and-indent)
+		 (define-key emacs-lisp-mode-map (kbd "C-c C-l") 'eval-buffer)))
 
 (provide 'arvid-lisp)
