702af7212f0d0f9218069b1bdea520ab64a12b99
Arvid Jakobsson <arvid@gluteus.se>
2010-09-13 09:03:32 +0200

    Semi-colon with ö.

--- arvid-css.el
+++ arvid-css.el
@@ -7,12 +7,14 @@
   ;; maybe do electric behavior
   (css-indent-line))
 
-(define-key css-mode-map "}" 'css-electric-brace)
 
 ;; Font lock for px, em, %, url-keywords and colors. From rejeep.
 (eval-after-load 'css-mode
   '(progn
-     (font-lock-add-keywords 'css-mode
+	 (define-key css-mode-map (kbd "ö") (lambda () (interactive (insert ";"))))
+	 (define-key css-mode-map (kbd ";") (lambda () (interactive) (message "Idiot!")))
+	 (define-key css-mode-map "}" 'css-electric-brace)
+	 (font-lock-add-keywords 'css-mode
                              '(("#[a-fA-F0-9][a-fA-F0-9][a-fA-F0-9]\\([a-fA-F0-9][a-fA-F0-9][a-fA-F0-9]\\)?" . font-lock-reference-face)
                                ("[0-9]+\\(px\\|em\\|%\\)" 1 font-lock-keyword-face)
                                ("\\(url\\)(" 1 font-lock-function-name-face)))))
