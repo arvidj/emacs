1ce187373b874e9a0f8e117d996c9af78d2f11fd
Arvid Jakobsson <arvid.jakobsson@gmail.com>
2010-09-23 00:48:04 +0200

    Spelling.

--- arvid-func.el
+++ arvid-func.el
@@ -15,7 +15,7 @@
     (message "Mark not active")))
 
 (defun nuke-all-buffers () 
-  "Kill all buffers, leaving *scractch* only."
+  "Kill all buffers, leaving *scratch* only."
   (interactive)
   (mapcar (lambda (x) (kill-buffer x)) (buffer-list)) 
   (delete-other-windows))
