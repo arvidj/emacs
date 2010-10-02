5a2cac6c7c08e68bb0e3b7987e67854d479c9b8d
Arvid Jakobsson <arvid.jakobsson@gmail.com>
2010-10-02 21:59:26 +0200

    Keybindings for creating new snippet and reloading all snippets.

--- arvid-yasnippet.el
+++ arvid-yasnippet.el
@@ -5,4 +5,7 @@
 (setq yas/root-directory "~/.emacs.d/snippets")
 (yas/load-directory yas/root-directory)
 
+(global-set-key (kbd "C-c yn") 'yas/new-snippet)
+(global-set-key (kbd "C-c yr") 'yas/reload-all)
+
 (provide 'arvid-yasnippet)
