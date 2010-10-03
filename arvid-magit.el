465854e4a79b7b8f8feb303486978ad4f809ee3b
Arvid Jakobsson <arvid.jakobsson@gmail.com>
2010-10-03 15:50:44 +0200

    Activate flyspell-mode when in magit-edit-log-mode. Courtesy of rejeep.

--- arvid-magit.el
+++ arvid-magit.el
@@ -7,6 +7,9 @@
 			(define-key magit-mode-map (kbd "C-o")
 			  (lambda () (interactive) (magit-visit-item -1)))))
 
+;; Add spell-checking when writing commit messages.
+(add-hook 'magit-log-edit-mode-hook (lambda () (flyspell-mode)))
+
 (global-set-key (kbd "C-x G") 'magit-status)
 
 (provide 'arvid-magit)
