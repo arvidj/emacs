3833b2970f2ffa235ba6cb337d2e5e57532552ed
Arvid Jakobsson <arvid@gluteus.se>
2010-10-04 09:17:52 +0200

    Remove obsolete binding in magit.

--- arvid-magit.el
+++ arvid-magit.el
@@ -1,11 +1,10 @@
 (require 'magit)
+(require 'magit-svn)
 
 (add-hook 'magit-mode-hook
           (lambda()
             (set-face-foreground 'magit-diff-add "green3")
-            (set-face-foreground 'magit-diff-del "red3")
-			(define-key magit-mode-map (kbd "C-o")
-			  (lambda () (interactive) (magit-visit-item -1)))))
+            (set-face-foreground 'magit-diff-del "red3")))
 
 (global-set-key (kbd "C-x G") 'magit-status)
 
