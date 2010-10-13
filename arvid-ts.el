08e2000d2ab263caeb67f3db0f2a1aa74e494acc
Arvid Jakobsson <arvid@gluteus.se>
2010-10-13 10:28:33 +0200

    Add some common TS-files to ts-mode auto-mode-alist.

--- arvid-ts.el
+++ arvid-ts.el
@@ -3,7 +3,18 @@
 
 ;;; TypoScript-mode
 (autoload 'ts-mode "ts-mode")
-(add-to-list 'auto-mode-alist '("\\.ts$\\|/ext_typoscript_setup\\.txt$" . ts-mode))
+
+(let ((ts-files-re (regexp-opt '(".ts"
+								 "/ext_typoscript_setup.txt"
+								 "/ext_typoscript_constants.txt"
+								 "/tsconfig_tinymce_common.txt"
+								 "/tsconfig_tinymce_fe.txt"
+								 "/tsconfig_tinymce_be.txt"
+								 "/typoscript_setup.txt"
+								 "/tsconfig_page.txt"))))
+  ;; What is up with the back tick monstrosity :(
+  (add-to-list 'auto-mode-alist `(,ts-files-re . ts-mode)))
+
 (add-hook 'ts-mode-hook 'my-ts-mode-hook)
 
 (defun my-ts-mode-hook () 
