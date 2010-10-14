84acbdb3feb39144f6b5d7268b4b9ce4dd57b292
Arvid Jakobsson <arvid@gluteus.se>
2010-10-14 09:16:43 +0200

    Fix iteration in fic-mode.

--- arvid-fic.el
+++ arvid-fic.el
@@ -1,15 +1,8 @@
 (require 'fic-mode)
 
-;; TODO add more modes
-
-;; TODO: this does not work since i dont understand proper quoting.
 (dolist (hook '(js-mode-hook c-mode-hook emacs-lisp-mode-hook ts-mode-hook))
-  (when (fboundp hook)
+  (when (boundp hook)
+	(print hook)
 	(add-hook hook 'turn-on-fic-mode)))
-
-(add-hook 'js-mode-hook 'turn-on-fic-mode)
-(add-hook 'c-mode-hook 'turn-on-fic-mode)
-(add-hook 'emacs-lisp-mode-hook 'turn-on-fic-mode)
-(add-hook 'ts-mode-hook 'turn-on-fic-mode)
 
 (provide 'arvid-fic)
