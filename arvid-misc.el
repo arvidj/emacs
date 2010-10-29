6b47b5101bfa8a8a309638b2d6c9c2160968ae34
Arvid Jakobsson <arvid@gluteus.se>
2010-10-29 09:04:58 +0200

    Remove whitespace when saving.

--- arvid-misc.el
+++ arvid-misc.el
@@ -1,3 +1,8 @@
+;;; Check out these modes for handling whitespace in a more ordered fashion.
+;; http://github.com/glasserc/ethan-wspace
+;; http://gist.github.com/452824
+
 (setq require-final-newline t)
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 (provide 'arvid-misc)
