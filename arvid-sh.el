eef3162eace4b4215cb0390f75893e68b3945271
Arvid Jakobsson <arvid@gluteus.se>
2010-12-08 10:08:00 +0100

    RET is newline-and-indent in sh-mode.

--- arvid-sh.el
+++ arvid-sh.el
@@ -1,6 +1,13 @@
 ;; Shell-script
+
+;; TODO Fix beginning-of-defun
 (add-hook 'sh-mode-hook
  	  '(lambda ()
-	     (setq sh-indent-comment t)))
+		 (setq sh-indent-comment t
+			   open-paren-in-column-0-is-defun-start nil)
+
+		 (define-keys
+		   sh-mode-map
+		   '(("RET" newline-and-indent)))))
 
 (provide 'arvid-sh)
