6e1c6e52e0559e74f24149d4e58183795b9add01
Arvid Jakobsson <arvid@gluteus.se>
2010-12-08 10:03:23 +0100

    Remove unused stuff from drag stuff mode.

--- arvid-drag-stuff.el
+++ arvid-drag-stuff.el
@@ -1,12 +1,6 @@
 (require 'drag-stuff)
 
 (drag-stuff-global-mode t)
-
 (setq drag-stuff-modifier '(meta shift))
-;; (define-key drag-stuff-mode-map (drag-stuff--kbd 'up) 'drag-stuff-up)
-(define-key drag-stuff-mode-map (kbd "\M-S-n") 'drag-stuff-down)
-;; (define-key drag-stuff-mode-map (drag-stuff--kbd 'down) 'drag-stuff-down)
-
-(define-key drag-stuff-mode-map (kbd "\M-S-p") 'drag-stuff-up)
 
 (provide 'arvid-drag-stuff)
