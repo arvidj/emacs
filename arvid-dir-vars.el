a9465c475c3bc305578b45fa072d95eb987b4884
Arvid Jakobsson <arvid@gluteus.se>
2010-12-08 10:02:52 +0100

    Setup dir locals.

--- arvid-dir-vars.el
+++ arvid-dir-vars.el
@@ -1,10 +1,26 @@
 ;;; TODO: When in gluteus live directory, switch color of modeline and
 ;;; set files as readonly.
 
-(dir-locals-set-class-variables
- 'gluteus
- '((nil . ((indent-tabs-mode t)
-           (default-tab-width . 4)))))
+(let ((tabs-okay '(remove 'tabs ethan-wspace-errors)))
+  (dir-locals-set-class-variables
+   'gluteus
+   `((nil . ((indent-tabs-mode . t)
+			 (ethan-wspace-errors . ,tabs-okay)
+			 (tab-width . 4)))))
+
+  (dir-locals-set-class-variables
+   'booksurfing
+   '((nil . ((indent-tabs-mode . nil)
+			 (tab-width . 4)
+			 (ethan-wspace-errors . (tabs eol no-nl-eof many-nls-eof))))))
+  (dir-locals-set-class-variables
+   'emacs
+   '((nil . ((indent-tabs-mode . nil)
+			 (tab-width . 4)
+			 (ethan-wspace-errors . (tabs eol no-nl-eof many-nls-eof))))))
+)
 
 (dir-locals-set-directory-class 
  (expand-file-name "~/dev/chrome-magenta-ext") 'gluteus)
+(dir-locals-set-directory-class
+ (expand-file-name "~/dev/booksurfing") 'booksurfing)
