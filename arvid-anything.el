22296683d2ae32b6d8b0422111e6c3becd8f7f7b
Arvid Jakobsson <arvid@gluteus.se>
2011-07-18 09:34:26 +0200

    Change anything prefix key to C-a, to not conflict with M-a for smart-beginning of line.

--- arvid-anything.el
+++ arvid-anything.el
@@ -1,7 +1,8 @@
 (require 'anything)
 
 ;; Default is f5 which conflicts with my binding for whitespace mode.
-(setq anything-command-map-prefix-key "M-a")
+(setq anything-command-map-prefix-key "C-a")
 (require 'anything-config)
+(global-set-key (kbd "C-a C-a") 'anything)
 
 (provide 'arvid-anything)
