68cf5f94a55d4dbb704ca11f88d98431a79e52dc
Arvid Jakobsson <arvid@gluteus.se>
2011-07-18 09:35:32 +0200

    Keybindings in Dired.

--- arvid-dired.el
+++ arvid-dired.el
@@ -4,8 +4,9 @@
 ;; TODO: Map over keybindings, see arvid-keys.el
 
 (define-key dired-mode-map (kbd "C-c e") 'wdired-change-to-wdired-mode)
-(define-key dired-mode-map (kbd "C-c u") 'dired-up-directory)
+(define-key dired-mode-map (kbd "I") 'dired-up-directory)
 (define-key dired-mode-map (kbd "C-c h") '(lambda () (interactive) (dired "~")))
-(define-key dired-mode-map (kbd "C-c h") '(lambda () (interactive) (dired "~")))
+
+;; http://www.emacswiki.org/emacs-en/DiredSortBySizeAndExtension
 
 (provide 'arvid-dired)
