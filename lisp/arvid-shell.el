dfafe7675b6d9cc8914cbe1c64f5c6cdd5f93239
Arvid Jakobsson <arvid.jakobsson@nomadic-labs.com>
2023-09-09 13:11:32 +0200

    shell-mode fix

--- lisp/arvid-shell.el
+++ lisp/arvid-shell.el
@@ -5,21 +5,17 @@ Used to emulate terminals C-l with a two-line prompt."
   (interactive)
   (recenter-top-bottom 1))
 
-(aj/define-keys
- shell-mode-map
- `(("C-l" aj/recenter-top1)
-   ("C-c C-o" browse-url-at-point)))
-
 (defun zsh-shell-mode-setup ()
   (setq-local comint-process-echoes t)
   (shell-dirtrack-mode -1)
   (dirtrack-mode -1)
   (add-hook 'comint-output-filter-functions #'comint-osc-process-output)
   ;; (setq 'comint-output-filter-functions '(comint-osc-process-output)
-  )
 
-
-
+  (aj/define-keys
+   shell-mode-map
+   `(("C-l" aj/recenter-top1)
+     ("C-c C-o" browse-url-at-point))))
 
 (add-hook 'shell-mode-hook #'zsh-shell-mode-setup)
 
