4b70d5d67dc83bfcc1d734940d774dc3b006d91d
Arvid Jakobsson <arvid.jakobsson@nomadic-labs.com>
2023-09-09 13:43:47 +0200

    add [elisp-autofmt]

--- lisp/arvid-lisp.el
+++ lisp/arvid-lisp.el
@@ -1,7 +1,14 @@
 ;;; elisp
 (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
-(add-hook 'emacs-lisp-mode-hook 
-      #'(lambda ()
-		 (define-key emacs-lisp-mode-map (kbd "C-c C-l") 'eval-buffer)))
+(add-hook
+ 'emacs-lisp-mode-hook
+ #'(lambda ()
+     (define-key emacs-lisp-mode-map (kbd "C-c C-l") 'eval-buffer)))
+
+(use-package
+ elisp-autofmt
+ :ensure t
+ :commands (elisp-autofmt-mode elisp-autofmt-buffer)
+ :hook (emacs-lisp-mode . elisp-autofmt-mode))
 
 (provide 'arvid-lisp)
