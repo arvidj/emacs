caee0546055d6f633e929c36b6e8d9bc84a48e00
Arvid Jakobsson <arvid.jakobsson@nomadic-labs.com>
2023-09-13 19:36:08 +0200

    Only elisp-autofmt load for emacs >= 29.1

--- lisp/arvid-lisp.el
+++ lisp/arvid-lisp.el
@@ -5,10 +5,13 @@
  #'(lambda ()
      (define-key emacs-lisp-mode-map (kbd "C-c C-l") 'eval-buffer)))
 
-(use-package
- elisp-autofmt
- :ensure t
- :commands (elisp-autofmt-mode elisp-autofmt-buffer)
- :hook (emacs-lisp-mode . elisp-autofmt-mode))
+;; TODO: hack since this package only works on emacs 29.1 and my
+;; laptop runs an older version.
+(if (version<= "29.1" emacs-version)
+    (use-package
+      elisp-autofmt
+      :ensure t
+      :commands (elisp-autofmt-mode elisp-autofmt-buffer)
+      :hook (emacs-lisp-mode . elisp-autofmt-mode)))
 
 (provide 'arvid-lisp)
