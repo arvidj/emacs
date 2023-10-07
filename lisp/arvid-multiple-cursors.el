c254e1d6d52fbea2d3eacf0f1fef81c57c0236f5
Arvid Jakobsson <arvid.jakobsson@nomadic-labs.com>
2023-10-07 15:27:20 +0200

    Comment out [ws-trim] integration in multiple cursors mode

--- lisp/arvid-multiple-cursors.el
+++ lisp/arvid-multiple-cursors.el
@@ -1,14 +1,13 @@
 
-(defun enable-disable-ws-trim ()
-  ""
-  (interactive)
-  (let ((enable
-         (if multiple-cursors-mode
-             0
-           1)))
-    (ws-trim-mode enable)
-    (electric-pair-mode enable)))
-
+;; (defun enable-disable-ws-trim ()
+;;   ""
+;;   (interactive)
+;;   (let ((enable
+;;          (if multiple-cursors-mode
+;;              0
+;;            1)))
+;;     (ws-trim-mode enable)
+;;     (electric-pair-mode enable)))
 
 
 (use-package
@@ -16,7 +15,7 @@
  :ensure t
  :config
  ;; ws-trim-mode && electric-pair messes with mc => turn it off.
- (add-hook 'multiple-cursors-mode-hook 'enable-disable-ws-trim)
+ ;; (add-hook 'multiple-cursors-mode-hook 'enable-disable-ws-trim)
  (define-key mc/keymap (kbd "C-i") 'mc/insert-numbers)
  (define-key mc/keymap (kbd "C-]") 'mc-hide-unmatched-lines-mode))
 
