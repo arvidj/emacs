e6e3118c72a1c743e9404a137880ca949fa6b03b
Arvid Jakobsson <arvid.jakobsson@nomadic-labs.com>
2024-02-24 10:43:50 +0100

    Remove windows package https stuff

--- lisp/arvid-package.el
+++ lisp/arvid-package.el
@@ -1,21 +1,8 @@
 (require 'package)
 (require 'use-package)
 
-(let* ((no-ssl
-        (and (memq system-type '(windows-nt ms-dos))
-             (not (gnutls-available-p))))
-       (proto
-        (if no-ssl
-            "http"
-          "https")))
-  (add-to-list 'package-archives
-               (cons "melpa" (concat proto "://melpa.org/packages/"))
-               t)
-  (add-to-list 'package-archives
-               (cons
-                "melpa-stable"
-                (concat proto "://stable.melpa.org/packages/"))
-               t))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
