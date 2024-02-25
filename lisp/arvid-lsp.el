bb4bce96ce9dc21e91c86abd875964263e4f0ed1
Arvid Jakobsson <arvid.jakobsson@nomadic-labs.com>
2024-02-25 07:13:13 +0100

    remove comments

--- lisp/arvid-lsp.el
+++ lisp/arvid-lsp.el
@@ -15,15 +15,6 @@
 
 ;; optionally
 (use-package lsp-ui :ensure t :commands lsp-ui-mode)
-;; if you are helm user
-;; (use-package helm-lsp :commands helm-lsp-workspace-symbol)
-;; if you are ivy user
-; ;(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
-;; (use-package lsp-treemacs :commands lsp-treemacs-errors-list)
-
-;; optionally if you want to use debugger
-;; (use-package dap-mode)
-;; (use-package dap-LANGUAGE) to load the dap adapter for your language
 
 ;; optional if you want which-key integration
 (use-package which-key :ensure t :config (which-key-mode))
